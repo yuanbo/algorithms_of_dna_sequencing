590314ca44cb488426a69729b7882153328e9136
Yuanbo Fan <yuanbo.fan@gmail.com>
2020-12-16 14:07:50 -0800

    update hw2, still missed 2 questions

--- ads1-hw-examples/subseq_index_query.py
+++ ads1-hw-examples/subseq_index_query.py
@@ -1,1 +1,12 @@
+from naive_2mm import naive_2mm
 def query_subseq(p, t, subseq_ind):
+    #hits = []
+    occurrences = set()
+    total_hits = 0
+    for i in range(3):
+        hits = subseq_ind.query(p[i:])
+        total_hits += len(hits)
+        for hit in hits:
+            if naive_2mm(p, t[hit-i:hit-i+len(p)]):
+                occurrences.add(hit-i)
+    return list(occurrences), total_hits
