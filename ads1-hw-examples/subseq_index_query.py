e5f8cb7b86f7ca4cf1d8458308b26fb9b45779b1
Yuanbo Fan <yuanbo.fan@gmail.com>
2020-12-17 15:24:05 -0800

    finish hw2

--- ads1-hw-examples/subseq_index_query.py
+++ ads1-hw-examples/subseq_index_query.py
@@ -1,6 +1,5 @@
 from naive_2mm import naive_2mm
 def query_subseq(p, t, subseq_ind):
-<<<<<<< HEAD
     #hits = []
     occurrences = set()
     total_hits = 0
@@ -11,6 +10,3 @@ def query_subseq(p, t, subseq_ind):
             if naive_2mm(p, t[hit-i:hit-i+len(p)]):
                 occurrences.add(hit-i)
     return list(occurrences), total_hits
-=======
-    return 0,0
->>>>>>> d8999357880a4bb0a26ebb7a174b19b8e95ca716
