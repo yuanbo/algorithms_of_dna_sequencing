5c1cba29ee5f5eeb026eb8cdbe6c4cf50a38479e
Yuanbo Fan <yuanbo.fan@gmail.com>
2020-12-18 23:17:34 -0800

    finish hw3

--- ads1-hw-examples/overlap.py
+++ ads1-hw-examples/overlap.py
@@ -1,22 +1,14 @@
-def editDistance(x, y):
-    # Create distance matrix
-    D = []
-    for i in range(len(x)+1):
-        D.append([0]*(len(y)+1))
-    # Initialize first row and column of matrix
-    for i in range(len(x)+1):
-        D[i][0] = i
-    for i in range(len(y)+1):
-        D[0][i] = i
-    # Fill in the rest of the matrix
-    for i in range(1, len(x)+1):
-        for j in range(1, len(y)+1):
-            distHor = D[i][j-1] + 1
-            distVer = D[i-1][j] + 1
-            if x[i-1] == y[j-1]:
-                distDiag = D[i-1][j-1]
-            else:
-                distDiag = D[i-1][j-1] + 1
-            D[i][j] = min(distHor, distVer, distDiag)
-    # Edit distance is the value in the bottom right corner of the matrix
-    return D[-1][-1]
+def overlap(a, b, min_length=3):
+    """ Return length of longest suffix of 'a' matching
+        a prefix of 'b' that is at least 'min_length'
+        characters long.  If no such overlap exists,
+        return 0. """
+    start = 0  # start all the way at the left
+    while True:
+        start = a.find(b[:min_length], start)  # look for b's prefix in a
+        if start == -1:  # no more occurrences to right
+            return 0
+        # found occurrence; check for full suffix/prefix match
+        if b.startswith(a[start:]):
+            return len(a)-start
+        start += 1  # move just past previous match
